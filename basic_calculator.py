565dfd1b83c99f18bf06d0b10fcbbce999bda903
SweggDoge <129165380+estban000@users.noreply.github.com>
2026-09-21 12:04:55 +0200

    Display white screen
    This commit was made to: Set up the display of the white screen in wich the results will be placed

--- basic_calculator.py
+++ basic_calculator.py
@@ -7,4 +7,14 @@ window.geometry("350x500")
 window.configure(bg="#4FBDF0")
 window.resizable(False, False)
 
+# Create the display screen of the calculator
+screen = tk.Entry(
+    window, font=("Arial", 22), justify="right",
+    bg="#ffffff",
+    fg="#000000",
+    insertbackground="black",
+    relief="flat", 
+)
+screen.pack(fill="x", padx=15, pady=20, ipady=12) # <= setting the size of the display screen 
+
 window.mainloop()
