713fd293d1804b480de4344af8d234aa99cf540f
SweggDoge <129165380+estban000@users.noreply.github.com>
2026-09-21 11:56:50 +0200

    Creation window Tkinter
    This commit was made to: Create the window & the background with the color #4FBDF0
    
    It's the start of this project :D

---
+++ basic_calculator.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+window.title("Python Calculator")
+window.geometry("350x500")
+window.configure(bg="#4FBDF0")
+window.resizable(False, False)
+
+window.mainloop()
